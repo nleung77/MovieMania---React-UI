13ee4d26c232bf681159714459248859558c9fc4
nleung77 <nleung77@gmail.com>
2023-04-23 08:41:52 -0400

    complete?

---
+++ src/MovieCard.jsx
@@ -0,0 +1,75 @@
+// import React from "react";
+
+// const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
+//   return (
+//     <div className="movie" key={imdbID}>
+//       <div>
+//         <p>{Year}</p>
+//       </div>
+
+//       <div>
+//         <img
+//           src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"}
+//           alt={Title}
+//         />
+//       </div>
+
+//       <div>
+//         <span>{Type}</span>
+//         <h3>{Title}</h3>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default MovieCard;
+
+import React, { useState } from "react";
+
+const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <div className="movie" key={imdbID}>
+      <div>
+        <p>{Year}</p>
+      </div>
+
+      <div>
+        <img
+          src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"}
+          alt={Title}
+          onClick={openModal}
+        />
+      </div>
+
+      <div>
+        <span>{Type}</span>
+        <h3>{Title}</h3>
+      </div>
+
+      {showModal && (
+        <div className="modal" onClick={closeModal}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <h2>{Title}</h2>
+            <p>{Type}</p>
+            <p>{Year}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MovieCard;
